7841235f27896eb78e4f5718e60dc67f16e265ea
David Zhang <david.zhang@wishabi.com>
2014-11-27 01:52:18 -0500

    Finished Dodecahedron, need to work on rotational axis as well as hover cover

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ $(document).ready(function () {
   $winWidth = $(window).width();
   $winHeight = $(window).height();
   $('.full-page').height($winHeight + 50);
-})
+
+  /* Quirky animations */
+  $('#intro').mouseenter(function() {
+    $(this).transition({
+      rotateX:'360deg'
+    }, 1000, 'easeInSine');
+  }).mouseout(function() {
+    $(this).transition({
+      rotateX:'0deg'
+    }, 1000, 'easeOutSine');
+  });
+
+  (function loop(){
+    $('#dodecahedron').transition({
+      rotateY:'+=10deg',
+    }, 1000, 'linear', loop);
+  })();
+});
 
 $(window).scroll(function() {
   $position = $(window).scrollTop();
@@ -16,3 +33,18 @@ $(window).scroll(function() {
     $header.addClass('animated fadeOut');
   }
 });
+
+
+// $('#intro').mouseover(function() {
+//   $(this).animate({
+//     -webkit-transform: rotateX(360),
+//     -moz-transform: rotateX(360),
+//     -ms-transform: rotateX(360)
+//   }, 2000, 'swing', function() {
+//     $(this).css({
+//       -webkit-transform: rotateX(0),
+//       -moz-transform: rotateX(0),
+//       -ms-transform: rotateX(0)
+//     })
+//   });
+// });
